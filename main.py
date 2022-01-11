d38b435b6a9ba4265de3cf5fbc8c9ad7438aa6d1
Andrew Macmillan <andrew.macmillan@ymail.com>
2022-01-11 20:18:29 +0000

    add Windows support

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 import argparse
 import os
 import pathlib
+import sys
 import tokenize
 
 from parser import PythonParser
@@ -27,17 +28,30 @@ def get_args() -> None:
     return args
 
 
-def write_html_file(html: str) -> None:
+def get_platform() -> None:
+    """
+    Returns the current platform for purposes of generating
+    the correct path for the output file
+    """
+    return sys.platform
+
+
+def write_html_file(html: str, platform: str) -> None:
     """
     Writes the html content generated from the PythonParser
     to a file and saves it in the current directory
 
     Args:
         html (str): the html to write to file
+        platform (str): the platform the program is running on
     """
     base_dir = os.path.abspath(os.path.dirname(__file__))
-    with open(f'{base_dir}/python_html.html', 'w+') as file:
-        file.write(html)
+    if platform == "linux" or platform == "darwin":
+        with open(f'{base_dir}/python_html.html', 'w+') as file:
+            file.write(html)
+    elif platform == "win32":
+        with open(f'{base_dir}\python_html.html', 'w+') as file:
+            file.write(html)
 
 
 def main() -> None:
@@ -45,14 +59,18 @@ def main() -> None:
     Main function for the Python_HTML_Parser app
     """
     args = get_args()
+    platform = get_platform()
     print('\n[+] Writing HTML from Python source...')
     with open(args.path, 'r') as file:
         tokens = tokenize.generate_tokens(file.readline)
         python_parser = PythonParser(tokens)
         html = python_parser.generate_html()
-        write_html_file(html)
+        write_html_file(html, platform)
         print('[+] Writing complete!')
-        print(f'\n[+] You can find your file here: {args.path}/python_html.html\n')
+        if platform == "linux" or platform == "darwin":
+            print(f'\n[+] You can find your file here: {args.path}/python_html.html\n')
+        elif platform == "win32":
+            print(f'\n[+] You can find your file here: {args.path}/python_html.html\n')
 
 
 if __name__ == '__main__':
