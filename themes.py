af503c823b168338a2c64fdc2bb21a7c416d0049
Andrew Macmillan <andrew.macmillan@ymail.com>
2022-01-21 20:34:16 +0000

    move CSS colour scheme to separate file

---
+++ themes.py
@@ -0,0 +1,59 @@
+COOL_BLUE = """
+    .code {
+        font-weight: bold;
+    }
+    .code-block {
+        display: flex;
+        flex-direction: column;
+        background-color: lightgrey;
+        border: 1px dashed #333;
+        text-align: left;
+        font-size: 1.1rem;
+        overflow-x: scroll;
+        white-space: nowrap;
+        padding: 20px;
+    }
+    .code-line {
+        margin: 3px 0;
+    }
+    .python-code-block {
+        background-color: #333;
+        margin: 40px 0;
+    }
+    .python-class,
+    .python-txt,
+    .python-func,
+    .python-op,
+    .python-str,
+    .python-str-prefix,
+    .python-keyword,
+    .python-special-keyword,
+    .python-comment,
+    .line-number {
+        font-size: 1.1rem;
+    }
+    .python-txt {
+        color: skyblue;
+    }
+    .python-op {
+        color: #fff;
+    }
+    .python-str {
+        color: tomato;
+    }
+    .python-str-prefix {
+        color: orange;
+    }
+    .python-keyword {
+        color: rgb(87, 62, 245);
+    }
+    .python-special-keyword {
+        color: rgb(159, 66, 253);
+    }
+    .python-comment {
+        color: rgb(25, 129, 25);
+    }
+    .line-number {
+        color: #666;
+    }
+    """
