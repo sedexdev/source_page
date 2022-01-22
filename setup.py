22666177a2ff8df7c638738dedb6d9ca9b197644
Andrew Macmillan <andrew.macmillan@ymail.com>
2022-01-22 21:48:08 +0000

    add setup.py for pip install - required creating pytml package in root

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+setup(
+    name='pytml',
+    version='1.0.0',
+    description='Parses Python source code into syntax highlighted HTML',
+    url='https://github.com/sedexdev/pytml',
+    author='Andrew Macmillan',
+    author_email='sedexdev@protonmail.com',
+    license='MIT',
+    packages=['pytml'],
+
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: End Users/Desktop',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+    ],
+)
