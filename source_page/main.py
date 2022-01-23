2d8562404c87c14025af1200c1451f5d32fff2b9
Andrew Macmillan <andrew.macmillan@ymail.com>
2022-01-23 19:40:43 +0000

    remove setup.py and update current directory check

--- source_page/main.py
+++ source_page/main.py
@@ -109,7 +109,8 @@ def write_html_file(html: str, plat: str) -> None:
         html (str): the html to write to file
         plat (str): the platform the program is running on
     """
-    base_dir = os.path.abspath(os.path.dirname(__file__))
+    base_dir = os.getcwd()
+    print(plat)
     if plat == "linux" or plat == "darwin":
         with open('{}/python_html.html'.format(base_dir), 'w') as file:
             file.write(html)
